25f874325adfc238d1c54fcbebfd7f5f875ec621
saeedamen <saeedamen@hotmail.com>
2020-06-14 11:32:56 +0100

    Updatings requirements

--- batch_scripts/linux/installation/install_pip_python_packages.sh
+++ batch_scripts/linux/installation/install_pip_python_packages.sh
@@ -16,22 +16,22 @@ if [ $TCAPY_PYTHON_ENV_TYPE == "virtualenv" ]; then
 
     # Install everything by pip
     pip install
-        setuptools-git==1.2 cython==0.29.13 arctic==1.79.2 sqlalchemy==1.3.7 redis==3.3.7 pymssql==2.1.4 \
-        pandas==0.24.2 numpy==1.16.4 scipy==1.3.1 statsmodels==0.10.1 tables==3.5.2 blosc==1.8.3 pyarrow==0.16.0 \
-        pathos==0.2.1 multiprocess==0.70.8 fastparquet==0.3.3 \
+        setuptools-git==1.2 cython==0.29.13 arctic==1.79.2 sqlalchemy==1.3.17 redis==3.3.7 pymssql==2.1.4 \
+        pandas==0.25.3 numpy==1.18.5 scipy==1.4.1 statsmodels==0.11.1 tables==3.5.2 blosc==1.8.3 pyarrow==0.16.0 \
+        pathos==0.2.1 multiprocess==0.70.9 fastparquet==0.3.3 \
         flask-restplus==0.13.0 gunicorn==19.9.0 \
-        beautifulsoup4==4.8.0 pdfkit==0.6.1 psutil==5.6.3 \
+        beautifulsoup4==4.8.0 pdfkit==0.6.1 psutil==5.6.6 \
         matplotlib==3.1.1 \
         boto3==1.5.11 \
         pyodbc==4.0.23 \
-        pytest==5.1.0 pytest-cov==2.5.1 \
+        pytest==5.4.3 pytest-cov==2.5.1 \
         mysql-connector-python==8.0.19 \
-        IPython==7.13.0 chartpy==0.1.8 findatapy==0.1.11 dash-auth==1.3.2 cufflinks==0.17.3 plotly==4.8.0 chart-studio==1.1.0 \
+        IPython==7.14.0 chartpy==0.1.8 findatapy==0.1.11 dash-auth==1.3.2 cufflinks==0.17.3 plotly==4.8.1 chart-studio==1.1.0 \
         dash==1.12.0 dash-html-components==1.0.3 dash-core-components==1.10.0 dash-table==4.7.0 jupyter-dash==0.2.1 dtale==1.8.1 \
         dtale==1.8.1 \
         qpython==2.0.0 influxdb==5.2.3 \
         Flask-Session==0.3.1 \
-        celery==4.4.0 pytest-tap kombu==4.6.7 python-memcached==1.59 numba==0.48.0 vispy==0.6.4 jinja2==2.11.1 \
+        celery==4.4.0 pytest-tap kombu==4.6.7 python-memcached==1.59 numba==0.48.0 vispy==0.6.4 jinja2==2.11.2 \
         jupyterlab jupyter_contrib_nbextensions jupyter_nbextensions_configurator RISE bqplot WeasyPrint==51 \
         dask==2.14.0 distributed==2.14.0 cloudpickle==1.3.0 python-snappy==0.5.4 bokeh==2.0.1 msgpack==1.0.0 pystore==0.1.15 fsspec==0.3.3
 
@@ -42,30 +42,29 @@ elif [ $TCAPY_PYTHON_ENV_TYPE == "conda" ] && [ $CONDA_FROM_YAML == 0 ]; then
 
     # Install conda forge packages
     conda install -c conda-forge \
-        setuptools-git=1.2 cython=0.29.13 arctic=1.79.2 sqlalchemy=1.3.7 redis-py=3.3.7 pymssql=2.1.4 \
-        pandas=0.24.2 numpy=1.16.4 scipy=1.3.1 statsmodels=0.10.1 pytables=3.5.2 python-blosc=1.8.3 \
-        pathos=0.2.1 multiprocess=0.70.8 fastparquet=0.3.3 \
+        setuptools-git=1.2 cython=0.29.13 arctic=1.79.2 sqlalchemy=1.3.17 redis-py=3.3.7 pymssql=2.1.4 \
+        pandas=0.25.3 numpy=1.18.5 scipy=1.4.1 statsmodels=0.11.1 pytables=3.5.2 python-blosc=1.8.3 \
+        pathos=0.2.1 multiprocess=0.70.9 fastparquet=0.3.3 \
         flask-restplus=0.13.0 gunicorn=19.9.0 \
         beautifulsoup4=4.8.0 python-pdfkit=0.6.1 psutil=5.6.3 \
         matplotlib=3.1.1 \
         boto3=1.5.11 \
         pyodbc=4.0.23 \
-        pytest=5.1.0 pytest-cov=2.5.1 \
-        numba=0.48.0 pyarrow=0.16.0 vispy=0.6.4 jinja2=2.11.1 \
+        pytest=5.4.3 pytest-cov=2.5.1 \
+        numba=0.48.0 pyarrow=0.16.0 vispy=0.6.4 jinja2=2.11.2 \
         jupyterlab jupyter_contrib_nbextensions jupyter_nbextensions_configurator nodejs rise bqplot \
         dask=2.14.0 distributed=2.14.0 cloudpickle=1.3.0 python-snappy=0.5.4 bokeh=2.0.1 msgpack-python=1.0.0 --yes
 
     # Install charting libraries
     # for flash recording of session variables
     # to allow celery to use Redis
-    pip install mysql-connector-python==8.0.19 chartpy==0.1.8 findatapy==0.1.11 dash-auth==1.3.2 cufflinks==0.17.3 plotly==4.8.0 \
+    pip install mysql-connector-python==8.0.19 chartpy==0.1.8 findatapy==0.1.11 dash-auth==1.3.2 cufflinks==0.17.3 plotly==4.8.1 \
         chart-studio==1.1.0 \
         dash==1.12.0 dash-html-components==1.0.3 dash-core-components==1.10.0 dash-table==4.7.0 jupyter-dash==0.2.1 dtale==1.8.1 \
         qpython==2.0.0 influxdb==5.2.3 \
         Flask-Session==0.3.1 \
         celery==4.4.0 pytest-tap kombu==4.6.7 python-memcached==1.59 WeasyPrint==51 pystore==0.1.15 fsspec==0.3.3
 
-    # celery[redis]==4.1.1 celery[msgpack]==4.1.1
     # To allow printing of Plotly to PDF/creation of PNG files
     conda install -c plotly plotly-orca=1.3.1 --yes
 fi
